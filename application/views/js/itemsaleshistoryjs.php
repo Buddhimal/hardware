90c8257944eb805d867d949fb70511c5fce290ba
ramesh201 <rameshsheran2@gmail.com>
2021-05-15 13:01:25 +0530

    item sales history report export buttons added

--- application/views/js/itemsaleshistoryjs.php
+++ application/views/js/itemsaleshistoryjs.php
@@ -22,7 +22,23 @@
 <script src="<?php echo base_url()?>plugins/datatables-buttons/js/buttons.html5.min.js"></script>
 <script src="<?php echo base_url()?>plugins/datatables-buttons/js/buttons.print.min.js"></script>
 <script src="<?php echo base_url()?>plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+<!-- DataTables -->
+<link rel="stylesheet" href="<?php echo base_url() ?>plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+<!-- DataTables -->
+<script src="<?php echo base_url() ?>plugins/datatables/jquery.dataTables.js"></script>
+<script src="<?php echo base_url() ?>plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url() ?>dist/js/jszip.min.js"></script>
+<script src="<?php echo base_url() ?>dist/js/adminlte.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.4.1/css/buttons.dataTables.min.css">
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.1/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.1/js/buttons.flash.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.1/js/buttons.html5.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.1/js/buttons.print.min.js"></script>
+
 <script>
+
     $('#reservation').daterangepicker();
    
      var qs = (function(a) {
@@ -42,9 +58,10 @@ $(document).ready(function(){
 
     $("#example1").DataTable({
       "responsive": true, "lengthChange": false, "autoWidth": false,"paging": true,
-      "searching": false,
+      "searching": true,
       "ordering": false,
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      dom: 'Bfrtip',
+    }).buttons('copy', 'excel','csv', 'print').container().appendTo('#example1_wrapper .col-md-6:eq(0)');
 });
 function openInvoiceModal(e){
     $('.modal-body').load('./invoiceModal?inv='+e.id.split('_')[1],function(){
