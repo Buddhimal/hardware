f07a5f3210f897b00dd31477172d20415ce2918e
ramesh201 <rameshsheran2@gmail.com>
2021-05-15 12:39:18 +0530

    Sales History Report

---
+++ application/views/saleshistoryreport.php
@@ -0,0 +1,89 @@
+
+<div class="wrapper">
+ 
+ <div class="content-wrapper">
+     <div class="content-header">
+         <div class="container-fluid">
+             <div class="row mb-2">
+             
+                 <div class="col-sm-8">
+                     <h3 class="m-0 text-dark" style="text-align:right;">Kithmi Holding Hardware Stores</h3>
+                     <h5 class="m-0 text-dark" style="text-align:right;">Building No, Street Name, City, Street,Zip</h5>
+                     <h5 class="m-0 text-dark" style="text-align:right;">Contact No: 011 255 6666 / 077 5677 722</h5>
+                 </div>
+                 <div class="col-sm-4">
+                     <ol class="breadcrumb float-sm-right">
+                         <li class="breadcrumb-item"><a href="#">Home</a></li>
+                         <li class="breadcrumb-item active">Sales History Report</li>
+                     </ol>
+                 </div>
+             </div>
+         </div>
+     </div>
+
+     <section class="content">
+         <div class="container-fluid">
+         <div class="row">
+         <div class="col-sm-4">
+                 </div>
+             <div class="col-sm-4" style="text-align:center;">
+                 <h5>Sales History Report</h5>
+                 </div>
+                 <div class="col-sm-4">
+                 </div>
+                 </div>
+             <div class="row">
+                 
+                 <div class="col-sm-12">
+                 <div class="card">
+        
+         <!-- /.card-header -->
+         <div class="card-body">
+           <table id="saleshistoryreporttable" class="table table-bordered table-striped">
+             <thead>
+             <tr>
+               <th style="text-aligh:left;">Item Code</th>
+               <th style="text-aligh:left;">Item Name</th>
+               <th style="text-aligh:left;">Item Group</th>
+               <th style="text-aligh:left;">Customer</th>
+               <th style="text-aligh:left;">Sales Date</th>
+               <th style="text-aligh:left;">Unit Price</th>
+               <th style="text-aligh:left;">Type</th>
+               <th style="text-aligh:left;">Sales Qty</th>
+               <th style="text-aligh:left;">Total Price (Rs.)</th>
+             </tr>
+             </thead>
+             <tbody>
+             <tr>
+             <td>1</td>
+             <td>2</td>
+             <td>3</td>
+             <td>4</td>
+             <td>5</td>
+             <td>6</td>
+             <td>7</td>
+             <td>8</td>
+             <td>9</td>
+             </tr>
+             <tr>
+             <td>10</td>
+             <td>2</td>
+             <td>3</td>
+             <td>4</td>
+             <td>5</td>
+             <td>6</td>
+             <td>7</td>
+             <td>8</td>
+             <td>9</td>
+             </tr>
+             
+           </table>
+         </div>
+      </div>
+         </div>
+         </div>
+
+         </div>
+     </section>
+ </div>
+
