9c67f41245554d7e4db04b90472e3761ebc79b07
Buddhimal Gunasekara <buddhimal.g@integritas.net>
2021-09-18 21:35:50 +0530

    fix for db

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/hardware/';
+$config['base_url'] = 'http://52.66.15.94/hardware/';
 
 /*
 |--------------------------------------------------------------------------
